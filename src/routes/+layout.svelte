c1d2d3ffaac909d82f0037c064a8b2e4c4278cd2
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2025-03-17 16:19:38 +0700

    feat: adjust padding for navbar and make homepage cute

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="container mx-auto p-4">
-    <nav class="mb-8 flex items-center space-x-4 border-b border-gray-200 pb-4">
+    <nav class="mb-2 flex items-center space-x-4 border-b border-gray-200 pb-2">
         <a
             href="/"
             class="text-lg font-medium text-blue-600 transition-colors duration-200 hover:text-blue-800"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1 class="m-2 p-12 text-2xl font-semibold">:3</h1>
+<div class="container mx-auto p-4">
+    <div class="flex flex-col items-center">
+        <img src="https://cdn.discordapp.com/emojis/1026533090627174460.png" alt=":3" class="m-2" />
+        <a
+            href="https://svelte.dev/"
+            target="_blank"
+            class="text-xl font-semibold text-slate-700 underline"
+        >
+            read if cute
+        </a>
+    </div>
+</div>
